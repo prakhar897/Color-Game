ad7781faaf5468a60d9dce303c33b74543ae30f7
Prakhar Gupta <prakhar897@gmail.com>
2018-05-16 13:46:25 +0530

    adafsfe

--- main.js
+++ main.js
@@ -1,24 +1,62 @@
-var colors = [
-	"rgb(255,0,0)",
-	"rgb(255,255,0)",
-	"rgb(0,255,0)",
-	"rgb(0,255,255)",
-	"rgb(0,0,255)",
-	"rgb(255,0,255)"
-]
+var colors = randomcolor();
+var pickedColor = colors[pickcolor()];
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
- var head = document.querySelector(".heading");
+ var head = document.querySelector("#heading");
  head.innerHTML = "Color Guessing Game " + pickedColor;
+ var ans = "";
+ var message = document.querySelector("#message");
+ var newgame = document.querySelector(".newgame");
+
 
 for(var i=0;i<squares.length;i++)
 {
 	squares[i].style.background = colors[i];
-	squares[i].addEventListeners("click",clicksq);
+	squares[i].addEventListener("click",clicksq);
 }
+
+newgame.addEventListener("click",generategame);
 
 function clicksq(){
 	var clickedcolor = this.style.background;
-	
+	console.log(clickedcolor);
+	if(clickedcolor == pickedColor)
+	{
+		for(i=0;i<squares.length;i++)
+		{
+			squares[i].style.background = pickedColor;
+		}
+		ans = "Correct!";
+		head.style.background = pickedColor;
+	}
+	else
+	{
+		ans = "Try Again!";
+		this.style.background = "black";
+	}
+	message.textContent = ans;
+
+}
+
+function randomcolor(){
+	var colors=[];
+	for(var i=0;i<6;i++)
+	{
+		colors[i] = 'rgb(' + Math.floor(Math.random()*255) + ", " + Math.floor(Math.random()*255) + ", " + Math.floor(Math.random()*255) + ')';
+	}
+	return colors;
+}
+
+function pickcolor(){
+	var e = Math.floor(Math.random()*6);
+	return e;
+}
+
+function generategame(){
+	colors = randomcolor();
+	pickedColor = colors[pickcolor()];
+	head.innerHTML = "Color Guessing Game " + pickedColor;
+	for(var i=0;i<squares.length;i++){
+		squares[i].style.background = colors[i];
+	}
 }
