56dc0cefe5d307ec3575ff2af062b4dfccd8d6c2
Prakhar Gupta <prakhar897@gmail.com>
2018-05-16 14:10:18 +0530

    adafsfe

--- main.js
+++ main.js
@@ -1,28 +1,32 @@
-var colors = randomcolor();
+var mode = 6;
+var colors = randomcolor(mode);
 var pickedColor = colors[pickcolor()];
-
 var squares = document.querySelectorAll(".square");
- var head = document.querySelector("#heading");
- head.innerHTML = "Color Guessing Game " + pickedColor;
- var ans = "";
- var message = document.querySelector("#message");
- var newgame = document.querySelector(".newgame");
+var head = document.querySelector("#heading");
+var ans = "";
+var message = document.querySelector("#message");
+var newgame = document.querySelector(".newgame");
+var easy = document.querySelector(".easy");
+var hard = document.querySelector(".hard");
 
+head.innerHTML = "Color Guessing Game " + pickedColor;
 
-for(var i=0;i<squares.length;i++)
+for(var i=0;i<mode;i++)
 {
 	squares[i].style.background = colors[i];
 	squares[i].addEventListener("click",clicksq);
 }
 
 newgame.addEventListener("click",generategame);
+easy.addEventListener("click",easyg);
+hard.addEventListener("click",hardg);
 
 function clicksq(){
 	var clickedcolor = this.style.background;
 	console.log(clickedcolor);
 	if(clickedcolor == pickedColor)
 	{
-		for(i=0;i<squares.length;i++)
+		for(i=0;i<mode;i++)
 		{
 			squares[i].style.background = pickedColor;
 		}
@@ -38,9 +42,9 @@ function clicksq(){
 
 }
 
-function randomcolor(){
+function randomcolor(num){
 	var colors=[];
-	for(var i=0;i<6;i++)
+	for(var i=0;i<num;i++)
 	{
 		colors[i] = 'rgb(' + Math.floor(Math.random()*255) + ", " + Math.floor(Math.random()*255) + ", " + Math.floor(Math.random()*255) + ')';
 	}
@@ -48,12 +52,34 @@ function randomcolor(){
 }
 
 function pickcolor(){
-	var e = Math.floor(Math.random()*6);
+	var e = Math.floor(Math.random()*mode);
 	return e;
 }
 
 function generategame(){
-	colors = randomcolor();
+	colors = randomcolor(mode);
+	pickedColor = colors[pickcolor()];
+	head.innerHTML = "Color Guessing Game " + pickedColor;
+	for(var i=0;i<mode;i++){
+		squares[i].style.background = colors[i];
+	}
+}
+
+function easyg(){
+	mode = 3;
+	colors = randomcolor(mode);
+	pickedColor = colors[pickcolor()];
+	head.innerHTML = "Color Guessing Game " + pickedColor;
+	for(var i=0;i<mode;i++){
+		squares[i].style.background = colors[i];
+	}
+	for(i=2;i<5;i++)
+		squares[i].style.display = "none";
+}
+
+function hardg(){
+	mode = 6;
+	colors = randomcolor(mode);
 	pickedColor = colors[pickcolor()];
 	head.innerHTML = "Color Guessing Game " + pickedColor;
 	for(var i=0;i<squares.length;i++){
