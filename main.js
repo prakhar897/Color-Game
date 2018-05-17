2480a818beb6b79eddc35905a9a3924efd63d130
Prakhar Gupta <prakhar897@gmail.com>
2018-05-17 19:27:27 +0530

    js debug

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-var mode = 6;
-var colors = randomcolor(mode);
-var pickedColor = colors[pickcolor()];
 var squares = document.querySelectorAll(".square");
 var head = document.querySelector("#heading");
-var ans = "";
 var message = document.querySelector("#message");
 var newgame = document.querySelector(".newgame");
 var easy = document.querySelector(".easy");
 var hard = document.querySelector(".hard");
 
-head.innerHTML = "Color Guessing Game " + pickedColor;
+var mode = 6;
+var ans = "";
+var colors;
+var pickedColor;
+
+generategame();
 
 for(var i=0;i<mode;i++)
 {
@@ -23,7 +24,7 @@ hard.addEventListener("click",hardg);
 
 function clicksq(){
 	var clickedcolor = this.style.background;
-	console.log(clickedcolor);
+	//console.log(clickedcolor);
 	if(clickedcolor == pickedColor)
 	{
 		for(i=0;i<mode;i++)
@@ -53,10 +54,14 @@ function randomcolor(num){
 
 function pickcolor(){
 	var e = Math.floor(Math.random()*mode);
+	//console.log(e);
 	return e;
 }
 
 function generategame(){
+	ans="";
+	message.textContent = ans;
+	head.style.background = "blue";
 	colors = randomcolor(mode);
 	pickedColor = colors[pickcolor()];
 	head.innerHTML = "Color Guessing Game " + pickedColor;
@@ -73,7 +78,7 @@ function easyg(){
 	for(var i=0;i<mode;i++){
 		squares[i].style.background = colors[i];
 	}
-	for(i=2;i<5;i++)
+	for(i=3;i<6;i++)
 		squares[i].style.display = "none";
 }
 
